7d012f7bd080e80e8859315091ceb8c4b652930c
dipoljoseph <97227464+dipoljoseph@users.noreply.github.com>
2023-06-17 17:36:40 +0800

    Add files via upload

---
+++ Ortigas Land/mobileres.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" type="text/css" href="mobileres1.css">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+
+    <title>Ortigas Land Mobile</title>
+</head>
+<body>
+    <!--navigation/header-->
+    <nav>
+    <input type="checkbox" id="check">
+    <label for="check" class="checkbtn">
+        <i class="fas fa-bars"></i>
+    </label>
+    <label class="logo"><img src="ortigas land/ortigasland copy 2.png" alt="header image" class="logo-navbar"></label>
+        <ul> 
+            <li><a href="#" class="active">Home</a></li>
+            <li><a href="#">About us</a></li>
+            <li><a href="#">Properties</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Events</a></li>
+            <li><a href="#">Contact Us</a></li>
+            <li><a href="#">Book A Discovery Call Today</a></li>
+        </ul>
+        
+
+        <div class="header-text">
+            <h1 class="h-name">MJ Marquez</h1>
+            <hr class="line">
+            <p class="h-des">The International Sales Manager</p>
+        </div>
+    </nav>
+    
+    <img src="ortigas land/layer 12.png" alt="header image" class="headerimage">
+
+    <!--about-->
+    <div class="about">
+        <div class="column">
+            <h1 class="left-col">About MJ</h1>
+            <br>
+            <p class="right-col">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            <br><br>
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+        </div>
+
+        <div class=featured-in>
+            <h1>Featured In</h1>
+            <p>Once a Sales and Marketing Specialist of LBC Mundial Corp. Ms. MJ found her new Luxurious job at Ortigas Land to help you bring your best life back home. 
+            <br><br>
+            When it comes to the best quality life style, MJ Marquez has it figured out. She knows that whether she’s in British Columbia or in the Philippines, she doesn’t have to compromise on anything. A lifestyle fit for only the best and brightest, like MJ herself, you know coming back home should be a total breeze. You deserve only the most serene of sanctuaries because home should feel like Home no matter where you are. With MJ Marquez bringing Ortigas Land to North America, it absolutely can’t get any better than this! On the 12th day of May year 2022, she was featured in the Philippine Canadian Inquirer (PCI) and also in Philippine Showbiz Today (PST) on 22nd day of May year 2022 .</p>
+        </div>
+
+        <div class="pic-column">
+            <img src="ortigas land/image-20220609-084940.png" alt="header image" class="feat-leftpic">
+
+            <img src="ortigas land/image-20220609-085050.png" alt="header image" class="feat-rightpic">
+        </div>
+    </div>
+
+    <!--cover-->
+    <div class="cover">
+        <img src="ortigas land/layer 13.jpg" alt="cover image" class="cover-pic">
+        <div class="column">
+            <h1 class="left-cov-text">Work with Ortigas Land Worldwide</h1>
+            <p class="right-cov-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            <br><br>
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+        </div>
+        <div class="btn">
+            <button class="view-more">VIEW MORE</button>
+        </div>
+    </div>
+
+    <!--feature projects-->
+    <div class="feature">
+        <h1 class="ft-h1">Featured Projects</h1>
+
+        <img src="ortigas land/dcfdad33-79bc-4d9c-93e3-e260de324a47.jpg" alt="feature image" class="ft-pic">
+
+        <h2 class="ft-h2">EMPRESS BY CAPITOL COMMONS</h1>
+
+        <p>Ortigas Land brings you a 56-storey residential tower in Capitol Commons, Pasig City. A place where residents can live, breathe, and take it easy. 
+        <br><br>
+        At Empress, we care about your sense of self, and growing within a community.</p>
+
+        <div class="btn-white">
+            <button class="view-more">VIEW MORE</button>
+        </div>
+
+        <img src="ortigas land/21976430-0fa9-44ed-ae41-58b390701ea9.jpg" alt="feature image" class="ft-pic">
+
+        <h2 class="ft-h2">THE GALLEON</h1>
+
+        <p>The Galleon is a premier development of Ortigas Land that serves as an ode of the company’s 8-decade legacy.
+        <br><br>
+        It is a two-tower, mixed-use development with an Office, Residential, and Retail products which is a unique offering within the heart of Ortigas central Business District.</p>
+
+        <div class="btn-white">
+            <button class="view-more" style="margin-bottom: 6%">VIEW MORE</button>
+        </div>
+    </div>
+
+    <!--client reviews-->
+    <div class="client-rev">
+        <h1 class="cr-h1">Client Reviews</h1>
+
+        <div class="review">
+            <img src="ortigas land/testi-image.png" alt="client review" class="cr-pic">
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+            <br><br>
+            CLIENT 001</p>
+
+            <p class="rating">
+                <img src="ortigas land/star.png" alt="client review" class="star">
+                <img src="ortigas land/star.png" alt="client review" class="star">
+                <img src="ortigas land/star.png" alt="client review" class="star">
+                <img src="ortigas land/star.png" alt="client review" class="star">
+                <img src="ortigas land/star.png" alt="client review" class="star">
+            </p>
+        </div>
+    </div>
+
+    <!--book-->
+    <div class="book">
+        <form action="/action_page.php">
+            <h2 class="book-h2">Book A Discovery Call Today</h2>
+
+            <p style="text-align: center">
+                <input type="text" id="fname" name="firstname" placeholder="Fullname">
+
+                <input type="text" id="lname" name="lastname" placeholder="Email Address">
+
+                <input type="text" id="lname" name="lastname" placeholder="Contact Number">
+
+                <input type="text" id="lname" name="lastname" placeholder="Message">
+            
+                <input type="submit" value="SUBMIT">
+            </p>
+        </form>
+    </div>
+
+    <!--footer-->
+    <div class="footer">
+        <div class="column">
+            <div class="f-leftcolumn">
+                <img src="ortigas land/layer 5.png" alt="client review" class="f-logo">
+
+                <h1>MJ Marquez</h1>
+                <p class="f-namedesc">The International Sales Manager</p>
+
+                <img src="ortigas land/fb-icon.png" alt="client review" class="soc-pic" style="width: 4%">
+                <img src="ortigas land/tw-icon.png" alt="client review" class="soc-pic">
+                <img src="ortigas land/yt-icon.png" alt="client review" class="soc-pic">
+                <img src="ortigas land/in-icon.png" alt="client review" class="soc-pic">
+            </div>
+
+            <div class="f-midcolumn">
+                <h3 class="f-menu">MENU</h3>
+                <a href="#">Home</a>
+                <a href="#">About Us</a>
+                <a href="#">Properties</a>
+                <a href="#">Blog</a>
+                <a href="#">Event</a>
+                <a href="#">Contact Us</a>
+            </div>
+            
+            <div class="f-rightcolumn">
+                <h3>QUICK CONTACT</h3>
+                <label>Address: </label>
+                <p>7F Estancia West Wing, Capitol Commons, Pasig City, Metro Manila, Philippines</p>
+                <label>Phone: </label>
+                <p>(+63) 961 833 0514 
+                +1 (604) 618 6461 (Viber/Whatsapp) </p>
+                <label>E-mail: </label>
+                <p>mj.marquez@sales-ortigas.com.ph</p>
+            </div>
+        </div>
+
+        <hr class="footer-line">
+
+        <p class="copyright">Copyright Ortigas Land 2022 . All rights reserved - Designed and Developed by 
+        <img src="ortigas land/itworks.png" alt="client review" class="c-logo">
+        </p>
+    </div>
+</body>
+</html>
---
+++ Ortigas Land/ortigasland.php
@@ -0,0 +1,269 @@
+<html>
+    <head>
+        <title>Ortigas Land</title>
+
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+    </head>
+
+<body>
+
+
+    <!--header-->
+
+    <div class="header">
+        <ul class="nav-bar">
+            <li><img src="ortigas land/ortigasland copy 2.png" alt="header image"></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About us</a></li>
+            <li><a href="#">Properties</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Events</a></li>
+            <li><a href="#">Contact Us</a></li>
+            <li><a href="#">Book a Discovery Call Today</a></li>
+        </ul>
+
+        <ul class="title-name">
+            <h1>MJ Marquez</h1>
+            <p>________________________________________________________</p>
+            <label>The International Sales Manager</label>
+        </ul>
+
+        <img src="ortigas land/layer 12.png" alt="header image" class="header-image">
+    </div>
+
+
+    <!--about-->
+
+    <div class="about">
+        <div class="column left"><h1>About MJ</h1></div>
+
+        <div class="column right">
+            <br><br>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            <br><br>
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+        </div>
+
+        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+        <div class="featured">
+            <h1>Featured In</h1>
+
+            <p>Once a Sales and Marketing Specialist of LBC Mundial Corp. Ms. MJ found her new Luxurious job at Ortigas Land to help you bring your best life back home. 
+            <br><br>
+            When it comes to the best quality life style, MJ Marquez has it figured out. She knows that whether she’s in British Columbia or in the Philippines, she doesn’t have to compromise on anything. A lifestyle fit for only the best and brightest, like MJ herself, you know coming back home should be a total breeze. You deserve only the most serene of sanctuaries because home should feel like Home no matter where you are. With MJ Marquez bringing Ortigas Land to North America, it absolutely can’t get any better than this! On the 12th day of May year 2022, she was featured in the Philippine Canadian Inquirer (PCI) and also in Philippine Showbiz Today (PST) on 22nd day of May year 2022 .</p>
+        </div>
+        
+        <br><br><br><br><br><br>
+
+        <div class="ft-photos">
+            <center>
+                <div class="column-ft left-ft">
+                    <img src="ortigas land/image-20220609-084940.png" alt="header image" class="left-ft">
+                </div>
+                <div class="column-ft right-ft">
+                    <img src="ortigas land/image-20220609-085050.png" alt="header image" class="right-ft">
+                </div>
+            </center>
+        </div>
+        
+        <br><br><br><br><br><br><br><br><br><br><br><br>
+    </div>
+
+
+    <!--cover-->
+
+    <div class="cover">
+        <center>
+            <img src="ortigas land/layer 13.jpg" alt="cover image" class="cover-pic">
+        </center>
+
+        <br><br><br><br>
+
+            <div class="column-info left-info">
+                <h1>Work with Ortigas Land Worldwide</h1> 
+            </div>
+
+            <div class="column-info right-info">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                <br><br>
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. 
+                </p>            
+            </div>
+
+            <br><br><br>
+
+            <center>
+                <button class="view-btn">VIEW MORE</button>
+            </center>
+
+            <br><br><br><br><br><br>
+    </div>
+
+
+    <!--featured projects-->
+
+    <div class="ft-proj">
+        <br><br><br><br>
+
+        <h1>Featured Projects</h1>
+
+        <br><br>
+
+        <center>
+            <img src="ortigas land/dcfdad33-79bc-4d9c-93e3-e260de324a47.jpg" alt="feature image" class="ft-pic">
+        </center>
+
+        <br><br><br>
+
+        <h2>EMPRESS BY CAPITOL COMMONS</h2>
+
+        <br>
+
+        <p>Ortigas Land brings you a 56-storey residential tower in Capitol Commons, Pasig City. A place where residents can live, breathe, and take it easy. 
+        <br><br>
+        At Empress, we care about your sense of self, and growing within a community.</p>
+
+        <br><br><br>
+
+        <center>
+            <button class="view-btn">VIEW MORE</button>
+        </center>
+
+        <br><br><br><br><br><br>
+
+        <center>
+            <img src="ortigas land/21976430-0fa9-44ed-ae41-58b390701ea9.jpg" alt="feature image" class="ft-pic">
+        </center>
+
+        <br><br><br>
+
+        <h2>THE GALLEON</h2>
+
+        <br>
+
+        <p>The Galleon is a premier development of Ortigas Land that serves as an ode of the company’s 8-decade legacy.
+        <br><br>
+        It is a two-tower, mixed-use development with an Office, Residential, and Retail products which is a unique offering within the heart of Ortigas central Business District.</p>
+
+        <br><br><br>
+
+        <center>
+            <button class="view-btn">VIEW MORE</button>
+        </center>
+
+        <br><br><br><br><br>
+
+    </div>
+
+
+    <!--client reviews-->
+    <div class="client-rev">
+        
+        <br><br><br><br><br>
+
+        <h1>Client Reviews</h1>
+
+        <br><br>
+        <center>
+            <img src="ortigas land/testi-image.png" alt="client review" class="ft-pic">
+
+            <br><br><br><br>
+            
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+            <br><br><br><br>
+            CLIENT 001</p>
+
+            <br>
+
+            <img src="ortigas land/star.png" alt="client review" class="ft-pic">
+            <img src="ortigas land/star.png" alt="client review" class="ft-pic">
+            <img src="ortigas land/star.png" alt="client review" class="ft-pic">
+            <img src="ortigas land/star.png" alt="client review" class="ft-pic">
+            <img src="ortigas land/star.png" alt="client review" class="ft-pic">
+
+            <br><br><br><br><br><br>
+        </center>
+
+    </div>
+
+
+    <!--client reviews-->
+    <div class="book">
+        <form action="/action_page.php">
+            <h1>Book A Discovery Call Today</h1>
+            <!--<label for="fname">First Name</label>-->
+
+            <center>
+                <input type="text" id="fname" name="firstname" placeholder="Fullname">
+
+                <input type="text" id="lname" name="lastname" placeholder="Email Address">
+
+                <input type="text" id="lname" name="lastname" placeholder="Contact Number">
+
+                <input type="text" id="lname" name="lastname" placeholder="Message" style="height: 200px; text-align=left">
+            
+                <input type="submit" value="Submit">
+                
+                <br><br><br><br>
+            </center>
+        </form>
+    </div>
+
+
+    <!--footer-->
+    <div class="footer">
+        <div class="column col-left">
+            <img src="ortigas land/layer 5.png" alt="client review" class="ft-pic">
+
+            <br><br><br><br>
+            <h1>MJ Marquez</h1>
+
+            <p>The International Sales Manager</p>
+
+            <br><br><br><br>
+            <img src="ortigas land/fb-icon.png" alt="client review" class="soc-pic">
+            <img src="ortigas land/tw-icon.png" alt="client review" class="soc-pic">
+            <img src="ortigas land/yt-icon.png" alt="client review" class="soc-pic">
+            <img src="ortigas land/in-icon.png" alt="client review" class="soc-pic">
+        </div>
+
+        <div class="column col-mid">
+            <h3>MENU</h3>
+            <br>
+            <a href="#">Home</a>
+            <a href="#">About Us</a>
+            <a href="#">Properties</a>
+            <a href="#">Blog</a>
+            <a href="#">Event</a>
+            <a href="#">Contact Us</a>
+        </div>
+
+        <div class="column col-right">
+            <h3>QUICK CONTACT</h3>
+            <br>
+            <p style="font-weight: bold;">Address: </p>
+            <p>7F Estancia West Wing, Capitol Commons, Pasig City, Metro Manila, Philippines</p>
+            <p style="font-weight: bold;">Phone: </p>
+            <p>(+63) 961 833 0514 
+            <br>
+            +1 (604) 618 6461 (Viber/Whatsapp) </p>
+            <br>
+            <p style="font-weight: bold;">E-mail: </p>
+            <p>mj.marquez@sales-ortigas.com.ph</p>
+        </div>
+
+        <hr class="spacer">
+
+        <center>
+            <hr class="line">
+            <p class="copyright">Copyright Ortigas Land 2022 &nbsp . &nbsp All rights reserved - Designed and Developed by
+            &nbsp
+            <img src="ortigas land/itworks.png" alt="client review" class="c-logo">
+            </p>
+        </center>
+    </div>
+
+</body>
+</html>
